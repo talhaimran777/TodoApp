388d903962edea15b72fa25a333f12816cadec18
Talha Imran <talhaimran284@gmail.com>
2020-09-29 21:29:25 +0500

    Setup bootstrap, jquery and served public folder to express to use these files

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Serving express static files like css, js and imgages
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.sendFile('./views/index.html', {root: path.resolve()});
 });
