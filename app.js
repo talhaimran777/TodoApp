3002e36e46685a007b4fbee61467035f86d14667
Talha Imran <talhaimran284@gmail.com>
2020-09-29 22:47:45 +0500

    Made some changes in the form element and implemented data send to the server through forms

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ const app = express();
 // Serving express static files like css, js and imgages
 app.use(express.static('public'));
 
+app.use(express.urlencoded({extended: false}));
+
 app.get('/', (req, res) => {
     res.sendFile('./views/index.html', {root: path.resolve()});
+});
+
+app.post('/submit', (req, res) => {
+    // redirect to  the home page
+    //res.redirect('/');
+    res.send('Your data was added to the database');
+    console.log('You Added: ', req.body.data);
 });
 
 
