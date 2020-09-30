0fd99b4c38837eebff44eddfb14adc933c29b1b8
Talha Imran <talhaimran284@gmail.com>
2020-09-30 22:14:15 +0500

    Reading Data from the database to the todo list app

--- app.js
+++ app.js
@@ -14,22 +14,34 @@ mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: tr
         db = client.db();
         console.log('Connected to the mongodb')
         app.listen(3000, () => console.log('Listening for requests'));
-
-        // db.collection('items').insertOne({});
     }
     else{
         console.log("Error Connecting to the data base");
     }
 });
 
+// Register a view engine
+app.set('view engine', 'ejs');
+
 // Serving express static files like css, js and imgages
 app.use(express.static('public'));
 
+// This is just for getting data passed through form and bind it in the request object
 app.use(express.urlencoded({extended: false}));
 
+
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: path.resolve()});
+    // res.sendFile('./views/index.html', {root: path.resolve()});
+
+    // Before rendering this you want to get all the data from the data base
+
+    db.collection('items').find().toArray((err, data) =>{
+        console.log("Data received from the mongodb:: ", data);
+
+        res.render('index', {data});
+    });
 });
+
 
 app.post('/create-item', (req, res) => {
 
