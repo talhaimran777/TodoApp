21b4bc19f9250641bf66d3e069e8280635e3316d
Talha Imran <talhaimran284@gmail.com>
2020-09-30 21:09:56 +0500

    C From CRUD operation of a data base done

--- app.js
+++ app.js
@@ -2,6 +2,26 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// require mongodb
+const mongodb = require('mongodb');
+
+let db;
+
+let connectionString = `mongodb+srv://talhaimran:talha157@cluster0.u8ext.gcp.mongodb.net/TodoApp?retryWrites=true&w=majority`;
+
+mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) =>{
+    if(!err){
+        db = client.db();
+        console.log('Connected to the mongodb')
+        app.listen(3000, () => console.log('Listening for requests'));
+
+        // db.collection('items').insertOne({});
+    }
+    else{
+        console.log("Error Connecting to the data base");
+    }
+});
+
 // Serving express static files like css, js and imgages
 app.use(express.static('public'));
 
@@ -11,12 +31,12 @@ app.get('/', (req, res) => {
     res.sendFile('./views/index.html', {root: path.resolve()});
 });
 
-app.post('/submit', (req, res) => {
-    // redirect to  the home page
-    //res.redirect('/');
-    res.send('Your data was added to the database');
-    console.log('You Added: ', req.body.data);
+app.post('/create-item', (req, res) => {
+
+    db.collection('items').insertOne({text: req.body.data}, () => {
+        res.redirect('/');
+    });
+    //console.log('You Added: ', req.body.data);
 });
 
 
-app.listen(3000, () => console.log('Listening for requests'));
